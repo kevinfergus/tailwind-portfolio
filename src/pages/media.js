045408cbb838953aaae85e4dbb4b95e5b6fdd8b9
Kevin Fergus <kevinfergus89@gmail.com>
2020-07-16 16:24:23 -0500

    vidoes centered

--- src/pages/media.js
+++ src/pages/media.js
@@ -6,19 +6,21 @@ function Media() {
 	return (
 		<Layout>
 			<div>
-				<div>
+				<section className="flex flex-row items-center md:flex-row">
 					<div>Code demo for gather</div>
 					<ReactPlayer
 						className="m-4"
 						url="https://www.youtube.com/watch?v=vl0p6XHA1vk&list=PLx0iOsdUOUmnf7I22qeTz8ms5tPM14dgg&index=18&t=0s"
 					/>
-				</div>
+				</section>
 				<div>
-					<div>Code demo for Hot Take</div>
-					<ReactPlayer
-						className="m-4"
-						url="https://www.youtube.com/watch?v=ZDr53uP2phE&feature=youtu.be"
-					/>{' '}
+					<section className="flex flex-row items-center md:flex-row">
+						<div>Code demo for Hot Take</div>
+						<ReactPlayer
+							className="m-4"
+							url="https://www.youtube.com/watch?v=ZDr53uP2phE&feature=youtu.be"
+						/>{' '}
+					</section>
 				</div>
 			</div>
 		</Layout>
