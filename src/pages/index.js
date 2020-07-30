cd95f6104d62431175f26dc4f13efcf132b3a9cd
Kevin Fergus <kevinfergus89@gmail.com>
2020-07-30 11:08:15 -0500

    playing with helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,38 +4,47 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import headshot from '../images/headshot.png';
 import { Link } from 'gatsby';
+import { Helmet } from 'react-helmet';
 
 function IndexPage() {
 	return (
-		<Layout>
-			<SEO keywords={[ `gatsby`, `tailwind`, `react`, `tailwindcss` ]} title="About" />
+		<div>
+			<Helmet>
+				<meta charSet="utf-8" />
+				<title>Kevin Fergus</title>
+				<link rel="canonical" href="kevinfergus.com" />
+			</Helmet>
 
-			<section className="flex flex-col items-center md:flex-row">
-				<div className="md:w-2/3 md:mr-8">
-					<div className="pl-4 leading-loose text-justify border-l-4 border-gray-900">
-						Hey there! My name is Kevin Fergus, I&apos;m a full stack software developer based in Chicago. I
-						like to create great user experiences and solve interesting problems. I&apos;m open to all
-						technologies but my tools of choice are Javascript, React, and Node.{' '}
-						<Link className="underline" to="/contact">
-							Let&apos;s make something cool.
-						</Link>
+			<Layout>
+				<SEO keywords={[ `gatsby`, `tailwind`, `react`, `tailwindcss` ]} title="About" />
+
+				<section className="flex flex-col items-center md:flex-row">
+					<div className="md:w-2/3 md:mr-8">
+						<div className="pl-4 leading-loose text-justify border-l-4 border-gray-900">
+							Hey there! My name is Kevin Fergus, I&apos;m a full stack software developer based in
+							Chicago. I like to create great user experiences and solve interesting problems. I&apos;m
+							open to all technologies but my tools of choice are Javascript, React, and Node.{' '}
+							<Link className="underline" to="/contact">
+								Let&apos;s make something cool.
+							</Link>
+						</div>
 					</div>
-				</div>
 
-				<figure className="w-2/3 md:w-1/3">
-					<img className="rounded" alt="headshot" src={headshot} />
-				</figure>
-				<ul className="list-none text-lg">
-					<li style={{ padding: 20 }}>HTML5</li>
-					<li style={{ padding: 20 }}>CSS3</li>
-					<li style={{ padding: 20 }}>Javascript</li>
-					<li style={{ padding: 20 }}>Node</li>
-					<li style={{ padding: 20 }}>React</li>
-					<li style={{ padding: 20 }}>Redux</li>
-					<li style={{ padding: 20 }}>PostgreSQL</li>
-				</ul>
-			</section>
-		</Layout>
+					<figure className="w-2/3 md:w-1/3">
+						<img className="rounded" alt="headshot" src={headshot} />
+					</figure>
+					<ul className="list-none text-lg">
+						<li style={{ padding: 20 }}>HTML5</li>
+						<li style={{ padding: 20 }}>CSS3</li>
+						<li style={{ padding: 20 }}>Javascript</li>
+						<li style={{ padding: 20 }}>Node</li>
+						<li style={{ padding: 20 }}>React</li>
+						<li style={{ padding: 20 }}>Redux</li>
+						<li style={{ padding: 20 }}>PostgreSQL</li>
+					</ul>
+				</section>
+			</Layout>
+		</div>
 	);
 }
 
