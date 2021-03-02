85accd536198bbe9338d09059ef0055488ca4157
Kevin Fergus <kevinfergus89@gmail.com>
2021-03-02 08:48:29 -0600

    removed hover from email and phone number, increased to 2xl font for links in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,21 +14,21 @@ function Layout({ children }) {
 			<footer className="bg-teal-700">
 				<nav className="flex justify-center max-w-4xl p-4 mx-auto text-sm md:p-8">
 				<a className="flex items-center text-white no-underline transform hover:-translate-y-1 hover:scale-110 m-5" href="https://www.linkedin.com/in/kevin-fergus/">
-						<span className="text-xl tracking-tight">LinkedIn</span>
+						<span className="text-2xl tracking-tight">LinkedIn</span>
 				</a>
 				<a className="flex items-center text-white no-underline transform hover:-translate-y-1 hover:scale-110 m-5" href="https://github.com/kevinfergus">
-						<span className="text-xl tracking-tight">GitHub</span>
+						<span className="text-2xl tracking-tight">GitHub</span>
 				</a>
 
 				<a className="flex items-center text-white no-underline transform hover:-translate-y-1 hover:scale-110 m-5" href={resume}>
-						<span className="text-xl tracking-tight">Resume</span>
+						<span className="text-2xl tracking-tight">Resume</span>
 				</a>
 
-				<a className="flex items-center text-white no-underline transform hover:-translate-y-1 hover:scale-110 m-5">
-						<span className="text-xl tracking-tight">(847)322-8527</span>
+				<a className="flex items-center text-white no-underline m-5">
+						<span className="tracking-tight">(847)322-8527</span>
 				</a>
-				<a className="flex items-center text-white no-underline transform hover:-translate-y-1 hover:scale-110 m-5">
-						<span className="text-xl tracking-tight"> kevinfergus89@gmail.com </span>
+				<a className="flex items-center text-white no-underline transform m-5">
+						<span className="tracking-tight"> kevinfergus89@gmail.com </span>
 				</a>
 
 
